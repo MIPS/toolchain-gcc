b24d8467fc60d67f010601ce8b5c360c8b603cc4
Andrew Hsieh <andrewhsieh@google.com>
2014-06-03 16:22:18 +0800

    [4.6] Backport fix to allow constexpr and const together
      2012-07-25  Jason Merrill  <jason@redhat.com>
        PR c++/54086
        * decl.c (grokdeclarator): Allow const and constexpr together.
    
    Change-Id: I6c2dfb287e3e84ad5bf3e5057f994560907367d9

--- gcc-4.6/gcc/cp/decl.c
+++ gcc-4.6/gcc/cp/decl.c
@@ -9144,8 +9144,6 @@ grokdeclarator (const cp_declarator *declarator,
      the object as `const'.  */
   if (constexpr_p && innermost_code != cdk_function)
     {
-      if (type_quals & TYPE_QUAL_CONST)
-        error ("both %<const%> and %<constexpr%> cannot be used here");
       if (type_quals & TYPE_QUAL_VOLATILE)
         error ("both %<volatile%> and %<constexpr%> cannot be used here");
       type_quals |= TYPE_QUAL_CONST;
