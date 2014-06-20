07c90b853afa81c102fbdcc3b956641f13c450c4
Pavel Chupin <pavel.v.chupin@intel.com>
2014-06-20 15:25:13 +0400

    Setup x86_64 ABI and add -mssse3 to x86 ABI
    32-bit: replace -msse3 by -mssse3
    64-bit: setup default options as -msse4.2 -mpopcnt
    
    Note: when multilib compiler is used -m32 will match 32-bit options and
    -m64 or default (neither -m32 nor -m64) will match 64-bit options.
    
    Change-Id: Ia20a03f54e3ff5857108e9ab0ae1c4c7c1e6cc7f
    Signed-off-by: Pavel Chupin <pavel.v.chupin@intel.com>

--- gcc-4.7/gcc/config/i386/gnu-user.h
+++ gcc-4.7/gcc/config/i386/gnu-user.h
@@ -84,7 +84,7 @@ along with GCC; see the file COPYING3.  If not see
 #define CC1_SPEC \
   LINUX_OR_ANDROID_CC (LINUX_TARGET_CC1_SPEC, \
                        LINUX_TARGET_CC1_SPEC \
-                       " -mstackrealign -msse3" \
+                       " -mstackrealign -mssse3" \
                        " -fno-short-enums" \
                        " " \
                        ANDROID_CC1_SPEC("-fPIC"))
